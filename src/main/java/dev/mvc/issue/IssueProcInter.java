0b103eb6890d93bef00ae3aaa6ab041463641b7a
Mr-Sung98 <elwlahs60@gmail.com>
2024-12-23 15:23:37 +0900

    Issue Paging Clear

--- src/main/java/dev/mvc/issue/IssueProcInter.java
+++ src/main/java/dev/mvc/issue/IssueProcInter.java
@@ -1,7 +1,10 @@
 package dev.mvc.issue;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
 
 public interface IssueProcInter {
   public int create(IssueVO issueVO);
@@ -10,4 +13,8 @@ public interface IssueProcInter {
   public int update(IssueVO issueVO);
   public int delete(int issueno);
   public int increaseCnt(int issueno);
+  public Integer list_search_count(String word);
+  public String pagingBox(int now_page, String word, String list_file_name, int search_count, int record_per_page,
+      int page_per_block);
+  public ArrayList<IssueVO> list_search_paging(String word, int now_page, int record_per_page);
 }

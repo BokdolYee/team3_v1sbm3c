b9dce304723ae7d279345cf93652b05ceb58d04a
BokdolYee <great1256@naver.com>
2024-12-30 20:18:59 +0900

    아이디, 비밀번호 찾기

---
+++ src/main/java/dev/mvc/mail/MailCont.java
@@ -0,0 +1,46 @@
+package dev.mvc.mail;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import dev.mvc.tool.MailTool;
+
+@Controller
+@RequestMapping("/mail")
+public class MailCont {
+  public MailCont() {
+    System.out.println("-> MailCont created.");
+  }
+
+  // http://localhost:9093/mail/form
+  /**
+   * 메일 입력 화면
+   * 
+   * @return
+   */
+  @GetMapping(value = "/form")
+  public String form(Model model) {
+
+    return "/mail/form";
+  }
+
+  // http://localhost:9093/mail/send
+  /**
+   * 메일 전송
+   * 
+   * @return
+   */
+  @PostMapping(value = "/send")
+  public String send(String receiver, String from, String title, String content) {
+    MailTool mailTool = new MailTool();
+    mailTool.send(receiver, from, title, content); // 메일 전송
+
+    return "/mail/sended";
+  }
+}
--- src/main/java/dev/mvc/team3/WebMvcConfiguration.java
+++ src/main/java/dev/mvc/team3/WebMvcConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 import dev.mvc.contents.Contents;
 import dev.mvc.survey.Survey;
 import dev.mvc.tool.Tool;
